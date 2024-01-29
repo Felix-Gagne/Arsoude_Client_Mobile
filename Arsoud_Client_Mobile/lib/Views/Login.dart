167802da46ca23e6c356a7fc8029e867f6282901
Vlad <2184803@cegepmontpetit.ca>
2024-01-29 09:13:05 -0500

    BUGIFX_Landsacpe : fix de la page login quand tu est en mode landscape

--- Arsoud_Client_Mobile/lib/Views/Login.dart
+++ Arsoud_Client_Mobile/lib/Views/Login.dart
@@ -43,127 +43,72 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         body:
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Login title and button
-            Container(
-              padding: EdgeInsets.fromLTRB(20, 30, 0, 0),
-              child: Row(
-                children: [
-                  IconButton(onPressed: (){
-
-                  }, icon: Icon(Icons.arrow_back_ios, color: Colors.black.withOpacity(0.5),), style: ButtonStyle(),),
-                  SizedBox(width: 12,),
-                  Text("Login", style: GoogleFonts.plusJakartaSans(
-                      textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
-                  ), )
-                ],
+        SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Login title and button
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 30, 0, 0),
+                child: Row(
+                  children: [
+                    IconButton(onPressed: (){
+          
+                    }, icon: Icon(Icons.arrow_back_ios, color: Colors.black.withOpacity(0.5),), style: ButtonStyle(),),
+                    SizedBox(width: 12,),
+                    Text("Login", style: GoogleFonts.plusJakartaSans(
+                        textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                    ), )
+                  ],
+                ),
               ),
-            ),
-            //Login Welcom title
-            Container(
-              padding: EdgeInsets.fromLTRB(24, 35, 168, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Welcome Back", style: GoogleFonts.plusJakartaSans(
-                      textStyle: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
-                  ),
-                  SizedBox(height: 20,),
-                  Text("Sign in to your account", style: GoogleFonts.plusJakartaSans(
-                      textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Color(
-                          0xFFC1C1C2))),)
-                ],
-              ),
-            ),
-            SizedBox(height: 36,),
-            //connexion information
-            Form(
-              key: _formKey,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(24, 0, 0, 0),
+              //Login Welcom title
+              Container(
+                padding: EdgeInsets.fromLTRB(24, 35, 168, 0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Email Address", style:  GoogleFonts.plusJakartaSans(
-                        textStyle: TextStyle(fontSize: 14, color:  Color(0xFF868687), fontWeight: FontWeight.w500)
-                    ),),
-                    SizedBox(height: 16,),
-                    //Email
-                    SizedBox(
-                      width: 320,
-                      //height: 48,
-                      child: TextFormField(
-                        validator: (email){
-                          if(email == ""){
-                            return "Please enter a email";
-                          }
-                          else{
-                            return null;
-                          }
-                        },
-                        controller: email,
-
-                        textAlignVertical: TextAlignVertical.center,
-                        textAlign: TextAlign.start,
-                        decoration: const InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 1.0, color: Color(0xFFCDCDD2)),
-                              borderRadius: BorderRadius.all(Radius.circular(36))
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 1.5, color: Color(0xFF797575)),
-                              borderRadius: BorderRadius.all(Radius.circular(36))
-                          ),
-                          errorBorder: OutlineInputBorder(
-                            borderSide: BorderSide(width: 1.5, color: Colors.red), // Customize error border color here
-                            borderRadius: BorderRadius.all(Radius.circular(36)),
-                          ),
-                          focusedErrorBorder: OutlineInputBorder(
-                            borderSide: BorderSide(width: 1.5, color: Colors.red), // Same as error border for consistency
-                            borderRadius: BorderRadius.all(Radius.circular(36)),
-                          ),
-
-                          contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        ),
-
-                      ),
+                    Text("Welcome Back", style: GoogleFonts.plusJakartaSans(
+                        textStyle: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
                     ),
-                    SizedBox(height: 24,),
-                    Text("Password", style:  GoogleFonts.plusJakartaSans(
-                        textStyle: TextStyle(fontSize: 14, color:  Color(0xFF868687), fontWeight: FontWeight.w500)
-                    )),
-                    SizedBox(height: 16,),
-                    //Password
-                    SizedBox(
-                      width: 320,
-                      //height: 48,
-                      child: TextFormField(
-                        validator: (password){
-                          if(password == ""){
-                            return "Please enter a password";
-                          }
-                          return null;
-                        },
-                        obscureText: showPassword,
-                        controller: password,
-                        textAlignVertical: TextAlignVertical.center,
-                        textAlign: TextAlign.start,
-                        decoration: InputDecoration(
-
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                  (showPassword ) ? Icons.visibility : Icons.visibility_off
-                              ),
-                              onPressed: (){
-                                showPassword = !showPassword;
-                                setState(() {
-
-                                });
-                              },
-                            ),
-
+                    SizedBox(height: 20,),
+                    Text("Sign in to your account", style: GoogleFonts.plusJakartaSans(
+                        textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Color(
+                            0xFFC1C1C2))),)
+                  ],
+                ),
+              ),
+              SizedBox(height: 36,),
+              //connexion information
+              Form(
+                key: _formKey,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(24, 0, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Email Address", style:  GoogleFonts.plusJakartaSans(
+                          textStyle: TextStyle(fontSize: 14, color:  Color(0xFF868687), fontWeight: FontWeight.w500)
+                      ),),
+                      SizedBox(height: 16,),
+                      //Email
+                      SizedBox(
+                        width: 320,
+                        //height: 48,
+                        child: TextFormField(
+                          validator: (email){
+                            if(email == ""){
+                              return "Please enter a email";
+                            }
+                            else{
+                              return null;
+                            }
+                          },
+                          controller: email,
+          
+                          textAlignVertical: TextAlignVertical.center,
+                          textAlign: TextAlign.start,
+                          decoration: const InputDecoration(
                             enabledBorder: OutlineInputBorder(
                                 borderSide: BorderSide(width: 1.0, color: Color(0xFFCDCDD2)),
                                 borderRadius: BorderRadius.all(Radius.circular(36))
@@ -180,54 +125,111 @@ class _MyHomePageState extends State<MyHomePage> {
                               borderSide: BorderSide(width: 1.5, color: Colors.red), // Same as error border for consistency
                               borderRadius: BorderRadius.all(Radius.circular(36)),
                             ),
+          
                             contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                            errorText: wrongInformationError.isNotEmpty ? wrongInformationError : null
+                          ),
+          
                         ),
-                        keyboardType: TextInputType.visiblePassword,
-                        textInputAction: TextInputAction.done,
-                        onChanged: (text) => setState(() => {}),
                       ),
-                    ),
+                      SizedBox(height: 24,),
+                      Text("Password", style:  GoogleFonts.plusJakartaSans(
+                          textStyle: TextStyle(fontSize: 14, color:  Color(0xFF868687), fontWeight: FontWeight.w500)
+                      )),
+                      SizedBox(height: 16,),
+                      //Password
+                      SizedBox(
+                        width: 320,
+                        //height: 48,
+                        child: TextFormField(
+                          validator: (password){
+                            if(password == ""){
+                              return "Please enter a password";
+                            }
+                            return null;
+                          },
+                          obscureText: showPassword,
+                          controller: password,
+                          textAlignVertical: TextAlignVertical.center,
+                          textAlign: TextAlign.start,
+                          decoration: InputDecoration(
+          
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                    (showPassword ) ? Icons.visibility : Icons.visibility_off
+                                ),
+                                onPressed: (){
+                                  showPassword = !showPassword;
+                                  setState(() {
+          
+                                  });
+                                },
+                              ),
+          
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 1.0, color: Color(0xFFCDCDD2)),
+                                  borderRadius: BorderRadius.all(Radius.circular(36))
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 1.5, color: Color(0xFF797575)),
+                                  borderRadius: BorderRadius.all(Radius.circular(36))
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 1.5, color: Colors.red), // Customize error border color here
+                                borderRadius: BorderRadius.all(Radius.circular(36)),
+                              ),
+                              focusedErrorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 1.5, color: Colors.red), // Same as error border for consistency
+                                borderRadius: BorderRadius.all(Radius.circular(36)),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                              errorText: wrongInformationError.isNotEmpty ? wrongInformationError : null
+                          ),
+                          keyboardType: TextInputType.visiblePassword,
+                          textInputAction: TextInputAction.done,
+                          onChanged: (text) => setState(() => {}),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              //Forgot button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 8, 35, 0),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      onPressed: (){},
+                      child: Text("Forgot password?", style: GoogleFonts.plusJakartaSans(
+                          textStyle: TextStyle(fontSize: 14, color:  Color(
+                              0xFF09635F), fontWeight: FontWeight.bold)
+                      ),)
+                  ),
+                ),
+              ),
+              //Login button
+              LoginButton(),
+              //Sign up section
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?", style: GoogleFonts.plusJakartaSans(
+                        textStyle: TextStyle(
+                            fontSize: 14, color:  Color(0xFF878788), fontWeight: FontWeight.w500
+                        ))),
+                    TextButton(
+                        onPressed: (){},
+                        child: Text("Sign up",style: GoogleFonts.plusJakartaSans(
+                            textStyle: TextStyle(
+                                fontSize: 14, color:  Color(0xFF09635F), fontWeight: FontWeight.bold
+                            ))))
                   ],
                 ),
-              ),
-            ),
-            //Forgot button
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 8, 35, 0),
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                    onPressed: (){},
-                    child: Text("Forgot password?", style: GoogleFonts.plusJakartaSans(
-                        textStyle: TextStyle(fontSize: 14, color:  Color(
-                            0xFF09635F), fontWeight: FontWeight.bold)
-                    ),)
-                ),
-              ),
-            ),
-            //Login button
-            LoginButton(),
-            //Sign up section
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have an account?", style: GoogleFonts.plusJakartaSans(
-                      textStyle: TextStyle(
-                          fontSize: 14, color:  Color(0xFF878788), fontWeight: FontWeight.w500
-                      ))),
-                  TextButton(
-                      onPressed: (){},
-                      child: Text("Sign up",style: GoogleFonts.plusJakartaSans(
-                          textStyle: TextStyle(
-                              fontSize: 14, color:  Color(0xFF09635F), fontWeight: FontWeight.bold
-                          ))))
-                ],
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         )
     );
   }
